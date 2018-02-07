a0a9b5d8646edd36544417a71ebf61e04f3bd328
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-07 18:20:08 +0100

    find name from own API, no template

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ module.exports = () => {
     const products = {
         items: [
             {
-                name: 'Abstract',
+                name:  'Abstract',
                 description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce aliquam vestibulum ipsum. Pellentesque arcu. Phasellus faucibus molestie nisl. Fusce tellus. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Pellentesque pretium lectus id turpis. Curabitur sagittis hendrerit ante. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur',
                 index: '225645ad55123wseqd'
             },
--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -6,6 +6,7 @@ export default class Search {
         this.container = container;
         this.searchInput = this.container.querySelector('.js-search-input');
         this.searchResult = this.container.querySelector('.js-search-result');
+        this.searchResultDesc = this.container.querySelector('.js-search-result--description');
 
         const jsTemplate = 'http://localhost:5001/js/templates';
         this.nunjEnv = nunjucks.configure(jsTemplate);
@@ -18,21 +19,47 @@ export default class Search {
             method: 'get',
             url: 'http://localhost:5003/products'
         })
-        .then((response) => {
-            this.renderResults(response.data);
-            console.info('Success!');
-        })
-        .catch((error) => {
-            console.error('Failed!');
-        });
+            .then((response) => {
+                this.renderResults(response.data);
+            })
+            .catch((error) => {
+                console.error('Failed!');
+            });
     }
 
     renderResults = data => {
-        const template = this.nunjEnv.getTemplate('result.nunj');
-        const insertTemplate = template.render({ data });
+        const items = data.items;
+        let NAME;
+        let ITEM;
+        let DESCRIPTION;
 
-        this.searchResult.innerHTML = insertTemplate;
+        items.forEach(item => {
+            const itenName = item.name;
+            const itenDescription = item.description;
+
+            DESCRIPTION = itenDescription;
+            NAME = itenName;
+
+            // ITEM = {
+            //     NAME,
+            //     DESCRIPTION
+            // };
+
+            this.findByName(DESCRIPTION, NAME);
+        });
+
         if (this.searchInput.value === '') this.searchResult.innerHTML = '';
-        console.log(this.searchInput.value);
+    }
+
+    findByName = (DESCRIPTION, NAME) => {
+        // const template = this.nunjEnv.getTemplate('result.nunj');
+
+        // const insertTemplate = template.render({ ITEM });
+
+        if (this.searchInput.value === NAME) {
+            this.searchResult.innerHTML = NAME;
+            console.log(DESCRIPTION);
+            this.searchResultDesc.innerHTML = DESCRIPTION;
+        }
     }
 }
