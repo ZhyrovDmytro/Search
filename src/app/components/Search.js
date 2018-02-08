9b2ef16fb2867b3135563ddec9595d46a607cccc
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-08 17:30:57 +0100

    works, code is very shit

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -8,6 +8,7 @@ export default class Search {
         this.searchResult = this.container.querySelector('.js-search-result');
         this.searchResultDesc = this.container.querySelector('.js-search-result--description');
 
+
         const jsTemplate = 'http://localhost:5001/js/templates';
         this.nunjEnv = nunjucks.configure(jsTemplate);
 
@@ -20,7 +21,6 @@ export default class Search {
             url: 'https://swapi.co/api/people/'
         })
             .then((response) => {
-                console.log(response.data.results);
                 this.findByName(response.data.results);
             })
             .catch((error) => {
@@ -28,26 +28,38 @@ export default class Search {
             });
     }
 
-    // renderResults = results => {
-    //     const myData = results.filter(selectData => {
-    //         this.findByName(selectData);
-
-    //         return selectData;
-    //     });
-    //     }
-
     findByName = (results) => {
         let NAME;
         const template = this.nunjEnv.getTemplate('result.nunj');
         const insertTemplate = template.render({ results });
+
         const myItem = results.forEach(item => {
             NAME = item.name;
         });
+        this.searchResult.innerHTML = insertTemplate;
 
-        if (NAME.toUpperCase().indexOf(this.searchInput.value.toUpperCase()) > -1) {
-            this.searchResult.innerHTML = insertTemplate;
-        }
+        this.renderResults();
 
         if (this.searchInput.value === '') this.searchResult.innerHTML = '';
+
+    }
+
+    renderResults = () => {
+        const table = this.container.querySelector('.js-search-table');
+        const rows = this.container.querySelectorAll('.js-search-row');
+        const item = this.container.querySelector('.js-search-item');
+        const itemName = this.container.querySelectorAll('.js-search-name');
+
+        for (let i = 0; i < rows.length; i += 1) {
+            const itemName = rows[i].querySelectorAll('.js-search-item')[0];
+            const findName = itemName.querySelector('.js-search-name');
+            if (itemName) {
+                if (findName.innerHTML.toUpperCase().indexOf(this.searchInput.value.toUpperCase()) > -1) {
+                    rows[i].style.display = '';
+                } else {
+                    rows[i].style.display = 'none';
+                }
+            }
+        }
     }
 }
