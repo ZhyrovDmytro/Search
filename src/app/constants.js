57a9c27152b20520690a3c5cae5dc91428ca2698
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-09 17:16:08 +0100

    refactored

---
+++ src/app/constants.js
@@ -0,0 +1,12 @@
+export const API = {
+    peopleFirstList: 'https://swapi.co/api/people/',
+    peopleSecondtList: 'https://swapi.co/api/people/?page=2'
+};
+
+export const template = {
+    templatePath: 'http://localhost:5001/js/templates'
+};
+
+export const state = {
+    disable: 'disabled'
+};
